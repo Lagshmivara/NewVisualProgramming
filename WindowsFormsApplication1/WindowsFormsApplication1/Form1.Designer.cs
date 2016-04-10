bc1b2c3d4fb4fb089fb84ae7c081f5a8f1603a8d
Lagshmivara <elena.shynkarenko@gmail.com>
2016-04-10 11:08:32 +0300

    ArrangedStudentsArray

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SearchResult = new System.Windows.Forms.ListBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Input smth to search";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // SearchResult
+            // 
+            this.SearchResult.FormattingEnabled = true;
+            this.SearchResult.Location = new System.Drawing.Point(12, 52);
+            this.SearchResult.Name = "SearchResult";
+            this.SearchResult.Size = new System.Drawing.Size(272, 173);
+            this.SearchResult.TabIndex = 1;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSearch.Location = new System.Drawing.Point(209, 9);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 2;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 302);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.SearchResult);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox SearchResult;
+        private System.Windows.Forms.Button buttonSearch;
     }
 }
 
--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -16,9 +16,69 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
     }
-    public class Student
+
+    class Student
     {
+        public Student(string n, string s, string b)
+        {
+            Name = n;
+            Surname = s;
+            Birthday = b;
+        }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Birthday { get; private set; }
+    }
+
+    class StudentsArray
+    {
+        object[] a;
+
+        public StudentsArray(int size)
+        {
+            a = new object[size];
+            Length = size;
+        }   
+        public int Length { get; set; }
+       
+        public object this[int index]
+        {
+            get
+            {
+                return a[index];
+            }
+            set
+            {
+                a[index] = value;
+            }
+        }
 
     }
+
+    class CreateStudent
+    {
+        public void Main()
+        {
+            StudentsArray stdnt = new StudentsArray(7);
+            stdnt[1] = new Student("Lena", "Shynkarenko", "19.03.98");
+            stdnt[2] = new Student("Yaroslav", "Nosov", "05.07.92");
+            stdnt[3] = new Student("Marina", "Ilinskaya", "20.11.89");
+            stdnt[4] = new Student("Mariya", "Ulitina", "13.02.95");
+            stdnt[5] = new Student("Anatoliy", "Kulakov", "25.12.94");
+            stdnt[6] = new Student("Roman", "Sidorov", "15.06.99");
+            stdnt[7] = new Student("Denis", "Shynkarenko", "02.09.84");
+        }
+    }
 }
+ 
